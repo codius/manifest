6d8ea909d33efd867678ddc537b1d4c010d3a280
Vernon Johnson <vernjohn@Vernons-MBP-2.localdomain>
2018-05-31 16:29:40 -0700

    fix: the hash private manifest variable function now returns a map

--- src/crypto-utils.js
+++ src/crypto-utils.js
@@ -4,17 +4,20 @@ const canonicalJson = require('canonical-json')
 const generateNonce = function () {
   // Generates 16 byte nonce
   const buf = crypto.randomBytes(16)
-  return buf.toString('base64')
+  return buf.toString('hex')
 }
 
 const hashPrivateManifestVars = function (manifest) {
   const privateVars = manifest['private']['vars']
+  const privateVarHashes = {}
 
-  return Object.keys(privateVars).map((key) => {
-    return crypto.createHash('sha256')
+  Object.keys(privateVars).map((key) => {
+    privateVarHashes[key] = crypto.createHash('sha256')
       .update(canonicalJson(privateVars[key]))
-      .digest('base64')
+      .digest('hex')
+    return key
   })
+  return privateVarHashes
 }
 
 exports.generateNonce = generateNonce

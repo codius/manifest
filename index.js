de6d6159afcf66274877af44d94e70e08cb3a72a
Vernon Johnson <vernjohn@Vernons-MBP-2.attlocal.net>
2018-06-01 22:24:03 -0700

    feat: export manifest validation functions via index.js entry point

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Index.js for Codius Manifest module
+const validateEnv = require('./src/validate-env.js').validateEnv
+const validatePrivateVars = require('./src/validate-private-vars.js').validatePrivateEnvVars
+const validatePublicVars = require('./src/validate-public-vars,js').validatePublicVars
+const validateContainers = require('./src/validate-containers.js').validateContainers
+const validateSchema = require('./src/validate-schema.js').validateSchema
+const validateManifest = require('./src/validate-manifest.js').validateManifest
+
+exports.validateEnv = validateEnv
+exports.validatePrivateVars = validatePrivateVars
+exports.validatePublicVars = validatePublicVars
+exports.validateSchema = validateSchema
+exports.validateContainers = validateContainers
+exports.validateManifest = validateManifest

204d8241bfd8d3f34936ef604be12b26cc67f7b9
Vernon Johnson <vtj2105@columbia.edu>
2018-06-13 18:06:07 -0700

    test: add tests for function that generates a complete manifest

---
+++ test/generate-complete-manifest.test.js
@@ -0,0 +1,76 @@
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+const { expect } = chai
+chai.use(chaiAsPromised)
+const fse = require('fs-extra')
+const { after, beforeEach, describe, it } = require('mocha')
+const { generateCompleteManifest } = require('../src/generate-complete-manifest.js')
+const validManifest = require('./mocks/manifest.test.json')
+const varsStatic = './test/mocks/codiusvars.test.json'
+const varsMock = './test/codiusvars.mock.json'
+const manifestStatic = './test/mocks/codius.test.json'
+const manifestMock = './test/mocks/codius.mock.json'
+const varsJson = fse.readJsonSync(varsStatic)
+const manifestJson = fse.readJsonSync(manifestStatic)
+
+describe('Generate Complete Manifest', function () {
+  beforeEach(async function () {
+    // Mock up test codiusvars.json and codius.json
+    await fse.writeJson(varsMock, varsJson)
+    await fse.writeJson(manifestMock, manifestJson)
+  })
+
+  after(async function () {
+    // Remove codiusvars.json and codius.json mock files
+    await fse.remove(varsMock)
+    await fse.remove(manifestMock)
+  })
+
+  it('should return the correct manifest when given valid codius files', async function () {
+    const result = await generateCompleteManifest(varsStatic, manifestStatic)
+    expect(result).to.deep.equal(validManifest)
+  })
+
+  it('should throw error if codius manifest has schema errors', async function () {
+    const manifest = JSON.parse(JSON.stringify(manifestJson))
+    delete manifest['manifest']['name']
+    await fse.writeJson(manifestMock, manifest)
+    const result = generateCompleteManifest(varsMock, manifestMock)
+    return expect(result).to.be.rejected
+  })
+
+  it('should throw error if codius vars has schema errors', async function () {
+    const vars = JSON.parse(JSON.stringify(manifestJson))
+    delete vars['vars']
+    await fse.writeJson(varsMock, vars)
+    const result = generateCompleteManifest(varsMock, manifestMock)
+    return expect(result).to.be.rejected
+  })
+
+  it('should override public variables that are already defined in codius manifest', async function () {
+    const manifest = JSON.parse(JSON.stringify(manifestJson))
+    manifest['manifest']['vars']['AWS_ACCESS_KEY'] = { value: 'ABSCEDADFSDSF' }
+    await fse.writeJson(manifestMock, manifest)
+    const result = generateCompleteManifest(varsMock, manifestMock)
+    return expect(result).to.eventually.become(validManifest)
+  })
+
+  it('should create the public encoding for a private var if one does not already exist', async function () {
+    const manifest = JSON.parse(JSON.stringify(manifestJson))
+    delete manifest['manifest']['vars']['AWS_SECRET_KEY']
+    await fse.writeJson(manifestMock, manifest)
+    const result = generateCompleteManifest(varsMock, manifestMock)
+    return expect(result).to.eventually.become(validManifest)
+  })
+
+  it('should override invalid public encodings for the private vars', async function () {
+    const manifest = JSON.parse(JSON.stringify(manifestJson))
+    manifest['manifest']['vars']['AWS_SECRET_KEY'] = {
+      'encoding': 'private:sha256',
+      'value': 'thisaninvalidhash'
+    }
+    await fse.writeJson(manifestMock, manifest)
+    const result = generateCompleteManifest(varsMock, manifestMock)
+    return expect(result).to.eventually.become(validManifest)
+  })
+})

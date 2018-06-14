072d9435ff0ce94b68e8eaec6e2c88f20af1b117
Vernon Johnson <vtj2105@columbia.edu>
2018-06-13 20:12:05 -0700

    feat: add top-level export for the generateCompleteManifest function

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Index.js for Codius Manifest module
 const { validateManifest } = require('./src/validate-manifest.js')
 const cryptoUtils = require('./src/common/crypto-utils.js')
-
+const { generateCompleteManifest } = require('./src/generate-complete-manifest.js')
 module.exports = {
   validateManifest,
+  generateCompleteManifest,
   generateNonce: cryptoUtils.generateNonce,
   hashManifest: cryptoUtils.hashManifest,
   hashPrivateVars: cryptoUtils.hashPrivateVars
